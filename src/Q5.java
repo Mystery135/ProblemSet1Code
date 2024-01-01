6738d676fd883086ae68c5e352645c8c496f7db9
Mystery135 <websiteemail12345678910@gmail.com>
2024-01-01 02:21:28 -0700

    Initial Commit

--- src/Q5.java
+++ src/Q5.java
@@ -1,2 +1,19 @@
 public class Q5 {
+    public static void main(String[] args) {
+        System.out.println(multiply(3, -1));;
+    }
+    public static int multiply(int x, int y){
+        if (y == 0){
+            return 0;
+        }
+
+        if (y > 0){
+            y--;
+            return x + multiply(x, y);
+        }
+            return -multiply(x, -y);
+        //+x * -y is equal to -(-x * +y).
+
+
+    }
 }
